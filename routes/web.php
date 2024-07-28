a8f5fb03468a92c931a6595cd8f2ec13b8ceb4e8
Fahad Ahmed <fahadahmedpro786@gmail.com>
2024-07-28 16:43:50 +0500

    ...:

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::get('/admin/login', function () {
 })->name('admin.login');
 
 Route::get('/admin', function () {
-    $applicants = Applicant::all();
+    $applicants = Applicant::distinct()->get();
     return view('admin.layout.app', compact('applicants'));
 })->name('admin.login');
